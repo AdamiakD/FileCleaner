43af84d004f5ef98e8f6eb21c869a7c7dbed969c
AdamiakD <Eliade1234$>
2022-09-22 22:26:52 +0200

    0.2

--- Program.cs
+++ Program.cs
@@ -10,20 +10,26 @@ namespace FileCleaner
         {
             /* Be careful! Your files and directories will be deleted irretrievably without test mode!
             Selecting TESTMODE = TRUE will make the program only display files without deleting them */ 
-            bool testMode = false;
+            bool testMode = true;
 
+            
             /* delExtensions indicates the extensions of the files to be deleted */
-            // string[] delExtensions = File.ReadAllLines($"{prgPath}/Config/delExtensions.ini");
+           
+            // string[] delExtensions = File.ReadAllLines($"{prgPath}/Config/Extensions.ini");
             string[] delExtensions = { ".XML", ".PGP", ".XLS", ".XLSX", ".CSV", ".DBF", ".TXT", ".ZIP", ".7Z", ".PDF", ".01", ".02" };
             
+            
             /* delExceptions is responsible for deletion exceptions */
-            // string[] delExceptions = File.ReadAllLines($"{prgPath}/Config/delExceptions.ini");
+            
+            // string[] delExceptions = File.ReadAllLines($"{prgPath}/Config/Exceptions.ini");
             string[] delExceptions = { "!", "IMPORTANT", "RAPORT", "RAPORT", "PROOF", "LAY" };
 
+            
             /* The first parameter in string[] setup is the number of days back from today. 
             The other parameters are resources to search for files to delete. */
+            
             // string[] setup = { "90", "d:\\Test1", "d:\\Test2" };
-            string[] setup = File.ReadAllLines($"{prgPath}/Config/setup.ini");
+            string[] setup = File.ReadAllLines($"{prgPath}/Config/Setup.ini");
 
 
             Cleaner cleanAgent = new Cleaner(setup[0], delExtensions, delExceptions);
