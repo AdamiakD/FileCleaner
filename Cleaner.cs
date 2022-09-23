b176f1187ad3f040b53ecb8f2e1f9e9bc3237daa
AdamiakD <109042555+AdamiakD@users.noreply.github.com>
2022-09-23 08:25:45 +0200

    0.2

--- Cleaner.cs
+++ Cleaner.cs
@@ -1,72 +1,89 @@
 using System;
 using System.Linq;
 using System.IO;
+using Microsoft.VisualBasic.FileIO;
 
 namespace FileCleaner
 {
     class Cleaner
     {
-        string daysCnt;
-        private static string[] deleteExt = { ".XML", ".PGP",".XLS", ".XLSX", ".CSV",".DBF",".TXT",".ZIP",".7Z",".PDF"};
-        private static string saveName = $"Raporty/{Program.timeNow.Year.ToString()}{Program.timeNow.Month.ToString().PadLeft(2, '0')}{Program.timeNow.Day.ToString().PadLeft(2, '0')}_raport.txt";
+        string daysCntBack;
+        static DateTime timeNow = DateTime.Now;
+        string[] delExtension;
+        string[] delException;
+        bool testMode;
 
-        public Cleaner(string daysCnt)
+        private static string saveFile = $"{timeNow.Year.ToString()}{timeNow.Month.ToString().PadLeft(2, '0')}{timeNow.Day.ToString().PadLeft(2, '0')}_report.txt";
+
+        public Cleaner(string daysCntBack, string[] delExtension, string[] delException, bool testMode)
         {
-            this.daysCnt = daysCnt;
+            this.daysCntBack = daysCntBack;
+            this.delExtension = delExtension;
+            this.delException = delException;
+            this.testMode = testMode;
         }
 
         public void DoClean(string searchDir)
         {
             try
             {
-                var filtredDirs = Directory.EnumerateDirectories(searchDir).Where(x => x.IndexOf("!") == -1 
-                    & x.ToUpper().IndexOf("LAY") == -1 & x.ToUpper().IndexOf("PROOF") == -1);
-                foreach (string d in filtredDirs)
+                var filtredFiles = Directory.EnumerateFiles(searchDir).Where(en => 
+                    !delException.Any(en.ToUpper().Contains) 
+                    & delExtension.Any(System.IO.Path.GetExtension(en).ToUpper().StartsWith));
+
+                foreach (string f in filtredFiles)
                 {
-                    
-                    var filtredFiles = Directory.EnumerateFiles(d).Where(x => x.IndexOf("!") == -1 
-                        & x.ToUpper().IndexOf("RAPORT") == -1 
-                        & x.ToUpper().IndexOf("PROOF") == -1 
-                        & deleteExt.Any(x.ToUpper().EndsWith) 
-                        | System.IO.Path.GetExtension(x).IndexOf(".0") > -1 
-                        | System.IO.Path.GetExtension(x).IndexOf(".1") > -1
-                        | System.IO.Path.GetExtension(x).IndexOf(".2") > -1);
-                    
-                    foreach (string f in filtredFiles)
+                    if (timeNow >= File.GetLastWriteTime(f).AddDays(Convert.ToInt32(this.daysCntBack)))
                     {
-                        // Console.WriteLine($"***{f}");
-                        DateTime fileData = File.GetLastWriteTime(f);
-                        if (Program.timeNow >= fileData.AddDays(Convert.ToInt32(this.daysCnt)))
+                        if(!testMode)
                         {
-                            string ext = System.IO.Path.GetExtension(f).ToUpper();
-                            if (!(ext.ToUpper() == ".PDF" & d.ToUpper().IndexOf("PODAJNIK") > 0))
-                            {
-                                this.DeleteFile(f, d);
-                            }
+                            FileSystem.DeleteFile(f, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                            this.WriteLog($"File: {f} - deleted");
+                        }
+                        else
+                        {
+                            this.WriteLog($"File: {f} - ready to delete");
                         }
                     }
-            
-                    this.DoClean(d);
-                    if (Directory.EnumerateDirectories(d).Count() + Directory.EnumerateFiles(d).Count() == 0)
+                }
+
+                if (Directory.EnumerateDirectories(searchDir).Count() + Directory.EnumerateFiles(searchDir).Count() == 0)
+                {
+                    if (!testMode)
                     {
-                        Console.WriteLine($"Dir {d} - deleted");
-                        Directory.Delete(d, false);
-                        File.AppendAllText($"{Program.prgPath}/{saveName}", $"{d}\n");
+                        FileSystem.DeleteDirectory(searchDir, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                        this.WriteLog($"Dir: {searchDir} - deleted");
+                    }
+                    else
+                    {
+                        this.WriteLog($"Dir: {searchDir} - ready to delete");
+                    }
+                }
+                else
+                {
+                    var filterdDirs = Directory.EnumerateDirectories(searchDir).Where(en =>
+                        !delException.Any(en.ToUpper().Contains));
+
+                    foreach (string d in filterdDirs)
+                    {
+                        this.DoClean(d);
                     }
                 }
             }
             catch (System.Exception excpt)
             {
-                Console.WriteLine($"***{excpt}");
-                File.AppendAllText($"{Program.prgPath}/log.log", $"{Program.timeNow.ToString()}  Exception:\n{excpt.ToString()}\n");
+                this.WriteLog($"*** Exception:\n{excpt.ToString()}");
             }
         }
 
-        private void DeleteFile(string f, string d)
+        public void WriteLog(string logMsg)
         {
-            Console.WriteLine($"File {f} - deleted");
-            File.Delete(f);
-            File.AppendAllText($"{Program.prgPath}/{saveName}", $"{f}\n");
+            if(!Directory.Exists($"{Program.prgPath}/Reports/"))
+            {
+                Directory.CreateDirectory($"{Program.prgPath}/Reports/");
+            }
+            Console.WriteLine(logMsg);
+            File.AppendAllText($"{Program.prgPath}/Reports/{saveFile}", $"{logMsg}\n");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace FileCleaner
 {
     class Program
     {
         public static string prgPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-        public static DateTime timeNow = DateTime.Now;
 
         static void Main(string[] args)
         {
-            File.AppendAllText($"{prgPath}/log.log", $"{timeNow.ToString()}  Start\n");
-            Console.WriteLine("Start");
+            /* Be careful! Your files and directories will be deleted to the Recycle Bin!
+            Selecting TESTMODE = TRUE will make the program only display files without deleting them */ 
+            
+            bool testMode = true;
 
-            string[] config = File.ReadAllLines($"{prgPath}\\config.ini");
             
-            Cleaner cleanAgent = new Cleaner(config[0]);
+            /* delExtensions indicates the extensions of the files to be deleted */
+           
+            // string[] delExtensions = File.ReadAllLines($"{prgPath}/Config/Extensions.ini");
+            string[] delExtensions = { ".XML", ".PGP", ".XLS", ".XLSX", ".CSV", ".DBF", ".TXT", ".ZIP", ".7Z", ".PDF", ".01", ".02" };
             
-            for (int i = 1; i < config.Length; i++)
+            
+            /* delExceptions is responsible for deletion exceptions */
+            
+            // string[] delExceptions = File.ReadAllLines($"{prgPath}/Config/Exceptions.ini");
+            string[] delExceptions = { "!", "IMPORTANT", "RAPORT", "RAPORT", "PROOF", "LAY" };
+
+            
+            /* The first parameter in string[] setup is the number of days back from today. 
+            The other parameters are resources to search for files to delete. */
+            
+            string[] setup = { "90", "d:\\work\\test" };
+            // string[] setup = File.ReadAllLines($"{prgPath}/Config/Setup.ini");
+
+
+            Cleaner cleanAgent = new Cleaner(setup[0], delExtensions, delExceptions, testMode);
+            cleanAgent.WriteLog($"Start");
+            
+            for (int i = 1; i < setup.Length; i++)
             {
-                cleanAgent.DoClean(config[i]);
+                cleanAgent.DoClean(setup[i]);
             }
-            File.AppendAllText($"{prgPath}/log.log", $"{timeNow.ToString()}  End\n");
-            Console.WriteLine("Finish");
+
+            cleanAgent.WriteLog($"End");
         }
     }
 }
